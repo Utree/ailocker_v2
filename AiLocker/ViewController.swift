ce162010d0cec32ca27ac316880561d2b4aa725a
Utree <yuukis322+github@gmail.com>
2019-03-18 19:00:29 +0900

    Initial Commit

---
+++ AiLocker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AiLocker
+//
+//  Created by sekiya on 2019/03/18.
+//  Copyright © 2019 sekiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
